8b766eaab35fb00d6a1e63416e1b39ad7e65eca9
Lorenzo Tosi <lorenzo.tosi10@studio.unibo.it>
2021-11-09 23:55:57 +0100

    exercise 5 done

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,24 +1,37 @@
 package it.unibo.oop.lab.nesting2;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class OneListAcceptable <T> implements Acceptable<T>{
 
 	List<T> l1;
 	
-	
-
 	public OneListAcceptable(List<T> l1) {
-		this.l1 = new ArrayList<>();
+		this.l1 = l1;
 	}
 
-
-
-	@Override
 	public Acceptor<T> acceptor() {
 		
-		return null;
+
+		
+		return new Acceptor<T>() {
+			
+			private int i=0;
+			
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				i++;
+				if(!l1.contains(newElement)) {
+					throw new ElementNotAcceptedException(newElement);
+				}
+			}
+			
+			public void end() throws EndNotAcceptedException {
+				if(l1.size()!=i) {
+					throw new EndNotAcceptedException();
+				}
+			}
+			
+		};
 	}
 
 }
