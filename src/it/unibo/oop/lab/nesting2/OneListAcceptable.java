626a537938da8ccc46956be264dee1297384f647
Lorenzo Tosi <lorenzo.tosi10@studio.unibo.it>
2021-11-09 22:19:44 +0100

    added constructor

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,8 +1,23 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OneListAcceptable <T> implements Acceptable<T>{
 
+	List<T> l1;
+	
+	
+
+	public OneListAcceptable(List<T> l1) {
+		this.l1 = new ArrayList<>();
+	}
+
+
+
+	@Override
 	public Acceptor<T> acceptor() {
+		
 		return null;
 	}
 
