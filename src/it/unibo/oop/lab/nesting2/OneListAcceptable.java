1c499b2152c717938914ae0fa7df851a3e4b8b4f
Lorenzo Tosi <lorenzo.tosi10@studio.unibo.it>
2021-11-09 21:44:46 +0100

    created new class

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,10 @@
+package it.unibo.oop.lab.nesting2;
+
+public class OneListAcceptable <T> implements Acceptable<T>{
+
+	public Acceptor<T> acceptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
